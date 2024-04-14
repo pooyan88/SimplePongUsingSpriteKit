d7a2fba0a2a3f31501fdec538b023b9d133095c4
Pooyan J <pooyanjavidi@gmail.com>
2024-04-14 12:28:18 +0330

    added some features.

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,17 +10,28 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    enum ImpulseState {
-        case startGame, mainGoals, enemyGoals
+    enum GameState {
+        case startGame, mainGoals, enemyGoals, pause
     }
     
     var ball = SKSpriteNode()
+    var impulseState: GameState?
     var mainPaddle = SKSpriteNode()
     var enemyPaddle = SKSpriteNode()
     var mainScoreLabel = SKLabelNode()
     var enemyScoreLabel = SKLabelNode()
-    var impulseState: ImpulseState?
-    var score: [Int] = []
+    var paddleContactCounter = 0 {
+        didSet {
+            if paddleContactCounter.isMultiple(of: 10) {
+                self.physicsWorld.speed += 0.2
+            }
+        }
+    }
+    var score: [Int] = [] {
+        didSet {
+            self.physicsWorld.speed = 1
+        }
+    }
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
@@ -92,21 +103,20 @@ extension GameScene {
         enemyScoreLabel.text = "0"
     }
     
-    private func startGame(state: ImpulseState) {
+    private func startGame(state: GameState) {
         switch state {
         case .enemyGoals:
             impulseState = .enemyGoals
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                self.ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
-            }
         case .mainGoals:
             impulseState = .mainGoals
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                 self.ball.physicsBody?.applyImpulse(CGVector(dx: -10, dy: -10))
             }
         case .startGame:
             impulseState = .startGame
             ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+        case .pause:
+            print("game has to pause")
         }
     }
     
@@ -124,6 +134,16 @@ extension GameScene {
         }
     }
     
+    private func scoreAction() {
+        if ball.position.y <= mainPaddle.position.y - mainPaddle.frame.height {
+            showEffectForScore(scorer: enemyPaddle)
+            addScore(winner: enemyPaddle)
+        } else if ball.position.y >= enemyPaddle.position.y + enemyPaddle.frame.height {
+            showEffectForScore(scorer: mainPaddle)
+            addScore(winner: mainPaddle)
+        }
+    }
+    
     private func addScore(winner: SKSpriteNode) {
         ball.position = CGPoint(x: 0, y: 0)
         ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0) // force stop the ball
@@ -134,7 +154,9 @@ extension GameScene {
         } else if winner == enemyPaddle {
             score[1] += 1
             moveBallTo(loser: mainPaddle)
-            startGame(state: .enemyGoals)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                self.startGame(state: .enemyGoals)
+            }
         }
         updateLabels()
         print(score)
@@ -143,10 +165,12 @@ extension GameScene {
     private func moveBallTo(loser: SKSpriteNode) {
         if loser == mainPaddle {
             let startPoint = CGPoint(x: mainPaddle.position.x, y: mainPaddle.position.y + 20)
-            ball.position = startPoint
+            let action = SKAction.moveBy(x: startPoint.x, y: startPoint.y, duration: 0)
+            ball.run(action)
         } else if loser == enemyPaddle {
             let startPoint = CGPoint(x: enemyPaddle.position.x, y: enemyPaddle.position.y - 20)
-            ball.position = startPoint
+            let action = SKAction.moveBy(x: startPoint.x, y: startPoint.y, duration: 0)
+            ball.run(action)
         }
     }
     
@@ -159,6 +183,16 @@ extension GameScene {
 // MARK: - Scene Delgate Functions
 extension GameScene: SCNPhysicsContactDelegate, SKPhysicsContactDelegate {
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        if let a = contact.bodyA.node?.name, let b = contact.bodyB.node?.name {
+            if a == ball.name || b == ball.name {
+                if a == enemyPaddle.name || b == enemyPaddle.name || a == mainPaddle.name || b == mainPaddle.name {
+                    paddleContactCounter += 1
+                }
+            }
+        }
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
@@ -170,6 +204,14 @@ extension GameScene: SCNPhysicsContactDelegate, SKPhysicsContactDelegate {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
+            if .enemyGoals == impulseState {
+                if location.x > mainPaddle.position.x {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 10))
+                } else {
+                    ball.physicsBody?.applyImpulse(CGVector(dx: -10, dy: -10))
+                }
+                impulseState = .startGame
+            }
             mainPaddle.run(SKAction.moveTo(x: location.x, duration: 0))
         }
     }
@@ -179,15 +221,8 @@ extension GameScene: SCNPhysicsContactDelegate, SKPhysicsContactDelegate {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 0.002))
-        
-        if ball.position.y <= mainPaddle.position.y - mainPaddle.frame.height {
-            showEffectForScore(scorer: enemyPaddle)
-            addScore(winner: enemyPaddle)
-        } else if ball.position.y >= enemyPaddle.position.y + enemyPaddle.frame.height {
-            showEffectForScore(scorer: mainPaddle)
-            addScore(winner: mainPaddle)
-        }
+        enemyPaddle.run(SKAction.moveTo(x: ball.position.x, duration: 0.05))
+        scoreAction()
     }
 }
 
